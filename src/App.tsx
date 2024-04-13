1c1303abdd4a2b536d620c042468546eceb3d7de
kosuke <sasaki.kosuke8@gmail.com>
2024-04-13 15:31:59 +0900

    課題2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ListPost from './ListPost';
+
+const App = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<ListPost />} />
+    </Route>
+  )
+);
 
 export default App;
+
