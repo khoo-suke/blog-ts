4c7df9bf56bc049b1b64e82d5a2e6b635ce99efb
kosuke sasaki <sasaki.kosuke8@gmail.com>
2024-04-14 15:42:56 +0900

    Revert "Test user1"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import './App.scss';
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import ListPost from './ListPost';
-
-const App = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<ListPost />} />
-    </Route>
-  )
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
-
